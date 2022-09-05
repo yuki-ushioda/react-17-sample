2c64c3980871287243da9f75660a4bf34fe760a4
潮田雄揮 <yuki-ushioda@exa-corp.co.jp>
2022-09-05 06:40:19 +0000

    Update 4 files

--- src/ChildArea.tsx
+++ src/ChildArea.tsx
@@ -1,11 +1,19 @@
 export const ChildArea = (props) =>{
     
+  const style = {
+      width: "100%",
+      height: "200px",
+      background: "khaki"
+      
+  }
   const { open } = props;
   return (
       <>
-      {open ? (<div>
-          <p>子コンポーネント</p>
-      </div>) : null}
+      {open ? (
+        <div style={style}>
+            <p>子コンポーネント</p>
+        </div>
+      ) : null}
       
       </>
   )
